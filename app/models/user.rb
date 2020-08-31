cf2796ad69cc7b34d94dadd2340c0109f3d6b4db
Dean Wheeler <dfwheeler394@gmail.com>
2020-08-31 09:50:19 -0500

    logic added for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # create user associations
+  has_many :tweets
+  has_many :favorites
+
+  #user is able to follower other users
+  has_many :followed_users, foreign_key: :follower_id, class_name: "Following"
+
+  # retrieve instances of the followed users in the followings table
+  has_many :followees, through: :followed_users, source: :followee
+
+  # the user has many followers
+  has_many :follower_users, foreign_key: :followed_id, class_name: "Following"
+
+  # retrive ID of users following the user
+  has_many :followers, through: :follower_users, source: :follower
 end
